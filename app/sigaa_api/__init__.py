ee8664f40cf024c2e72214e3d699363f130f145b
max.cohen <maxwell.cohen@sentecamaldita.com.br>
2025-12-04 02:52:16 -0300

    render fix 0.1

--- app/sigaa_api/__init__.py
+++ app/sigaa_api/__init__.py
@@ -1,3 +1,14 @@
-from .sigaa import Sigaa
+from flask import Flask
+import os
 
-__all__ = ['Sigaa']
+
+def create_app():
+    app = Flask(__name__)
+
+    # Use variável de ambiente em produção
+    app.secret_key = os.environ.get('SECRET_KEY', 'some_secret_key_for_session')
+
+    from . import routes
+    app.register_blueprint(routes.bp)
+
+    return app
